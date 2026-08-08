ac7e53f88685183d9180d152fb1092d1c55cfc0a
Unnikrishnan U R <unni24061@gmail.com>
2026-08-08 21:20:48 +0530

    Prepare project for Render deployment

--- collegePortal/settings.py
+++ collegePortal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k*qc8l@9e4ehk*ry8-b@suqh-l52*v*oqxi8qnb^)73twg(m$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
